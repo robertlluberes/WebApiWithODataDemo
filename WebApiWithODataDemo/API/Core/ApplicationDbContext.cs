bebfd89ceb8d51a509ef4d8de2e3f615c0561bf5
robertlluberes <robertlluberes@gmail.com>
2019-07-21 22:46:44 -0400

    Add DbContext

---
+++ WebApiWithODataDemo/API/Core/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using API.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Core
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Community> Communities { get; set; }
+
+        public DbSet<Technology> Technologies { get; set; }
+
+        public DbSet<Member> Members { get; set; }
+
+        public DbSet<CommunityMember> CommunityMembers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CommunityMember>()
+                .HasKey(cm => new { cm.CommunityId, cm.MemberId });
+
+            modelBuilder.Entity<CommunityMember>()
+                .HasOne(cm => cm.Community)
+                .WithMany(c => c.CommunityMembers)
+                .HasForeignKey(cm => cm.CommunityId);
+
+            modelBuilder.Entity<CommunityMember>()
+                .HasOne(cm => cm.Member)
+                .WithMany(m => m.CommunityMembers)
+                .HasForeignKey(cm => cm.MemberId);
+        }
+    }
+}
